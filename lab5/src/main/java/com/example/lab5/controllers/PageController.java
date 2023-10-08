83e5771ab7ee6fd9515dc8dfa2216c22743c085f
dzytko <dawid.zytko@gmail.com>
2023-10-08 16:26:07 +0200

    lab5: task 1

---
+++ lab5/src/main/java/com/example/lab5/controllers/PageController.java
@@ -0,0 +1,19 @@
+package com.example.lab5.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String mainPage() {
+        return "Hello Spring Boot from mainPage() method!";
+    }
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String pageTwo() {
+        return "Hello Spring Boot from pageTwo() method!";
+    }
+}
