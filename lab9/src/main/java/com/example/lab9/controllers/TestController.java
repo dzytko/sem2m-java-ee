1484233a177902ba8803b3c9c3ce90eec045334b
dzytko <dawid.zytko@gmail.com>
2023-12-10 23:42:39 +0100

    lab9: fix mapping in test controller

--- lab9/src/main/java/com/example/lab9/controllers/TestController.java
+++ lab9/src/main/java/com/example/lab9/controllers/TestController.java
@@ -1,11 +1,12 @@
 package com.example.lab9.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String welcomePage() {
         return "Welcome!";
     }
